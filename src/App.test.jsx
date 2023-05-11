7bc2c0d4b4f517132774c055ef092093abe1fd07
wakana <wakanawardrobe@gmail.com>
2023-05-11 11:51:45 +0100

    commit initial files

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import { ... } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App component", () => {
+    it("renders correct heading", () => {
+        render(<App />);
+        expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+    });
+});
